f197e26ea786a54b2a25060fa425157da351028c
asadbekme <asadbekme2002@gmail.com>
2025-03-09 12:34:08 +0500

    feat: added copy clipboard

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 const currentYearDom = document.getElementById("getwolt-current-year");
 const currentYear = new Date().getFullYear();
 currentYearDom.innerText = currentYear;
+
+function copyCode() {
+  const codeElement = document.getElementById("referral-code");
+  const code = codeElement.textContent;
+  navigator.clipboard
+    .writeText(code)
+    .then(() => {
+      alert("Code copied to clipboard!");
+    })
+    .catch((err) => {
+      console.error("Failed to copy code: ", err);
+    });
+}
